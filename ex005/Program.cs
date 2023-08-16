0c0fd5931b238931c54f3739299199a207dd6e0d
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 14:53:19 -0300

    Add files via upload

---
+++ ex005/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+int num;
+int fatorial = 1;
+
+Console.WriteLine("Informe um numero: ");
+
+while (true)
+{
+    num = int.Parse(s: Console.ReadLine());
+    if (num < 0)
+    {
+        Console.WriteLine("Numero invalido, informe outro: ");
+        continue;
+    }
+    else
+    {
+        break;
+    }
+}
+
+for (int i = 2; i <= num; i++)
+{
+    fatorial *= i;
+}
+Console.WriteLine(fatorial);
