37679c43a628926ba56e1af91b35f8db314434bc
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 15:36:08 -0300

    Add files via upload

---
+++ Ex06.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ex006
+{
+    class Ex06
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[100];
+            int cont = 0;
+
+            Console.WriteLine("Informe um inteiro: (Para encerrar dgite um negativo)");
+
+            for (int i = 0; i < 100; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+                if (num[i] < 0)
+                {
+                    break;
+                }
+                cont++;
+            }
+
+            Console.WriteLine("Números digitados:");
+
+            for (int i = 0; i < cont; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+        }
+    }
+}
