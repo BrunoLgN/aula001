91189035b9b6f65174c4585455c030eb1b0d6d41
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 13:55:01 -0300

    ex002

---
+++ ex002/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace orientacao_objetos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("Informe um numero: ");
+        num = int.Parse(Console.ReadLine());
+        if(num < 0)
+        {
+            Console.WriteLine("ERRO");
+        }
+
+        
+        while(num >= 0)
+        {
+            Console.WriteLine(num + "...");
+            num--;
+        }
+    }
+}
