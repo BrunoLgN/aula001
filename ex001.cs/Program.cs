3c3244013b59a141246aef4051934a5789edffa2
Bruno Oliveira <brunodeoliveira0409@gmail.com>
2023-08-16 12:27:51 -0300

    ex001

---
+++ ex001.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿float nota1, nota2, media;
+Console.Write("Informe primeira nota: ");
+nota1 = float.Parse(Console.ReadLine());
+
+Console.Write("Informe segunda nota: ");
+nota2 = float.Parse(Console.ReadLine());
+
+media = (nota1 + nota2) / 2;
+if(media >= 7)
+{
+    Console.WriteLine("Aluno aprovado com média: {0}", media);
+} else 
+{
+    Console.WriteLine("Aluno reprovado com média: {0}", media);
+}
+Console.ReadKey();
